6ef8aeb8de832c14f101b1aef0955698b631e8fb
tarjei <tarjei.skjaerset@gmail.com>
2020-01-27 12:17:17 +0100

    Clean arrangemntSvcClient

--- app/src/api/arrangementSvc.ts
+++ app/src/api/arrangementSvc.ts
@@ -4,60 +4,50 @@ import { WithId } from 'src/types';
 import { IParticipantContract, IParticipant } from 'src/types/participant';
 import { getArrangementSvcUrl } from 'src/config';
 
-export const postEvent = async (event: IEvent) => {
-  return await post({
+export const postEvent = (event: IEvent) =>
+  post({
     host: getArrangementSvcUrl(),
     path: '/events',
     body: serializeEvent(event),
   }).then(response => response as WithId<IEventContract>);
-};
 
-export const putEvent = async (
+export const putEvent = (
   eventId: string,
   event: IEvent
-): Promise<IEventContract> => {
-  return await put({
+): Promise<IEventContract> =>
+  put({
     host: getArrangementSvcUrl(),
     path: `/events/${eventId}`,
     body: serializeEvent(event),
   }).then(response => response as IEventContract);
-};
 
-export const getEvent = async (eventId: string): Promise<IEventContract> => {
-  return await get({
+export const getEvent = (eventId: string): Promise<IEventContract> =>
+  get({
     host: getArrangementSvcUrl(),
     path: `/events/${eventId}`,
   }).then(response => response as IEventContract);
-};
 
-export const getEvents = async () => {
-  return await get({
+export const getEvents = () =>
+  get({
     host: getArrangementSvcUrl(),
     path: `/events`,
   }).then(response => response as WithId<IEventContract>[]);
-};
 
-export const deleteEvent = async (eventId: string) => {
-  await del({
+export const deleteEvent = (eventId: string) =>
+  del({
     host: getArrangementSvcUrl(),
     path: `/events/${eventId}`,
   });
-};
 
-export const postParticipant = async (participant: IParticipant) => {
-  return await post({
+export const postParticipant = (participant: IParticipant) =>
+  post({
     host: getArrangementSvcUrl(),
-    path: `/participant/${participant.email}/events/${participant.eventId}`,
+    path: `/events/${participant.eventId}/participants/${participant.email}`,
     body: {},
   }).then(response => response as IParticipantContract);
-};
 
-export const deleteParticipant = async (
-  eventId: string,
-  participantEmail: string
-) => {
-  return await del({
+export const deleteParticipant = (eventId: string, participantEmail: string) =>
+  del({
     host: getArrangementSvcUrl(),
-    path: `/participant/${participantEmail}/events/${eventId}`,
+    path: `/events/${eventId}/participants/${participantEmail}`,
   });
-};
