8f5f8ebf0fd7f756835b631aa842cd55eed7363a
Ida Bosch <idabosch@gmail.com>
2020-01-28 11:23:30 +0100

    :sparkles: Show date and time on desktop

--- app/src/components/ViewEvents/EventListElement.tsx
+++ app/src/components/ViewEvents/EventListElement.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { IEvent } from 'src/types/event';
 import { Link } from 'react-router-dom';
 import style from './EventListElement.module.scss';
-import { stringifyDate } from 'src/types/date';
+import { stringifyDate, isSameDate } from 'src/types/date';
+import { stringifyTime } from 'src/types/time';
 
 interface IProps {
   event: IEvent;
@@ -10,11 +11,21 @@ interface IProps {
 }
 
 export const EventListElement = ({ event, onClickRoute }: IProps) => {
+  const dateText = isSameDate(event.start.date, event.end.date)
+    ? `${stringifyDate(event.start.date)}`
+    : `${stringifyDate(event.start.date)} \n - ${stringifyDate(
+        event.end.date
+      )}`;
+  const desktopTimeText = `${stringifyTime(event.start.time)} - ${stringifyTime(
+    event.end.time
+  )}`;
+
   return (
-    <div className={style.container}>
+    <div>
       <Link to={onClickRoute} className={style.link}>
         <div>{event.title}</div>
-        <div className={style.date}>{stringifyDate(event.start.date)}</div>
+        <div className={style.date}>{dateText}</div>
+        <div className={style.desktopDate}> {desktopTimeText}</div>
       </Link>
     </div>
   );
