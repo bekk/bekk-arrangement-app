178f417eb8bbb8565ec9450bb2e52de065884f95
tarjei <tarjei.skjaerset@gmail.com>
2020-03-10 14:27:45 +0100

    Fix time left to render immediately

--- app/src/hooks/timeleftHooks.ts
+++ app/src/hooks/timeleftHooks.ts
@@ -2,21 +2,19 @@ import { useState, useEffect } from 'react';
 import { calculateTimeLeft } from 'src/utils/timeleft';
 
 export const useTimeLeft = (time: Date | false) => {
-  const [timeLeft, setTimeLeft] = useState({
-    days: 0,
-    hours: 0,
-    minutes: 0,
-    seconds: 0,
-    difference: 1,
-  });
+  const [timeLeft, setTimeLeft] = useState(
+    calculateTimeLeft(time || new Date())
+  );
 
   useEffect(() => {
     if (time) {
+      setTimeLeft(calculateTimeLeft(time));
       const interval = setInterval(() => {
         setTimeLeft(calculateTimeLeft(time));
       }, 1000);
       return () => clearInterval(interval);
     }
   }, [time]);
+
   return timeLeft;
 };
