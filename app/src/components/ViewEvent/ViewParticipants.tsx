731211b1d807887f1ccf4f926a2fb4ff1b678747
sarahjelle <sarahj.98@hotmail.com>
2021-06-24 12:26:47 +0200

    Fix empty participant list feedback message
    Co-authored-by: Tarjei Skjærset <tskj@tarjei.org>

--- app/src/components/ViewEvent/ViewParticipants.tsx
+++ app/src/components/ViewEvent/ViewParticipants.tsx
@@ -12,19 +12,16 @@ interface IProps {
 export const ViewParticipants = ({ eventId, editToken }: IProps) => {
   const remoteParticipants = useParticipants(eventId, editToken);
 
-  const attendees = hasLoaded(remoteParticipants)
-    ? remoteParticipants.data.attendees
-    : [];
-  const waitingList = hasLoaded(remoteParticipants)
-    ? remoteParticipants.data.waitingList
-    : [];
+  if (!hasLoaded(remoteParticipants)) {
+    return <div>Laster...</div>;
+  }
 
   return (
     <div>
       <h1 className={style.subHeader}>Påmeldte</h1>
       <div>
-        {attendees.length > 0 ? (
-          attendees.map((attendee) => {
+        {remoteParticipants.data.attendees.length > 0 ? (
+          remoteParticipants.data.attendees.map((attendee) => {
             return (
               <div key={stringifyEmail(attendee.email)} className={style.text}>
                 {attendee.name}, {stringifyEmail(attendee.email)}, Kommentar:{' '}
@@ -35,10 +32,10 @@ export const ViewParticipants = ({ eventId, editToken }: IProps) => {
         ) : (
           <div className={style.text}>Ingen påmeldte</div>
         )}
-        {waitingList && waitingList.length > 0 && (
+        {remoteParticipants.data.waitingList && remoteParticipants.data.waitingList.length > 0 && (
           <>
             <h3>På venteliste</h3>
-            {waitingList.map((waitlisted) => (
+            {remoteParticipants.data.waitingList.map((waitlisted) => (
               <div
                 key={stringifyEmail(waitlisted.email)}
                 className={style.text}
