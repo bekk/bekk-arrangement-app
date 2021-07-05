4d69a9b55edc52c3bcd3f040f813ab62b60fd53a
sarahjelle <sarahj.98@hotmail.com>
2021-07-05 13:26:57 +0200

    Fix review comments

--- app/src/components/ViewEventsCards/EventCardElement.tsx
+++ app/src/components/ViewEventsCards/EventCardElement.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { IEvent } from 'src/types/event';
 import { Link } from 'react-router-dom';
 import style from './EventCardElement.module.scss';
@@ -12,6 +12,7 @@ import classNames from 'classnames';
 import { EventState } from 'src/components/ViewEventsCards/EventState';
 import { isInThePast } from 'src/types/date-time';
 import { isNumber } from 'lodash';
+import { LocationIcon } from 'src/components/Common/Icons/LocationIcon';
 
 interface IProps {
   eventId: string;
@@ -46,7 +47,7 @@ export const EventCardElement = ({ eventId, event }: IProps) => {
     event.maxParticipants === 0
       ? 'Plass'
       : numberOfParticipants === undefined
-      ? 'loading'
+      ? 'Laster'
       : numberOfParticipants < event.maxParticipants
       ? 'Plass'
       : numberOfParticipants >= event.maxParticipants && event.hasWaitingList
@@ -63,10 +64,7 @@ export const EventCardElement = ({ eventId, event }: IProps) => {
 
   const viewRoute = viewEventRoute(eventId);
 
-  const [svgColor, setSvgColor] = useState('White');
-
-  const titleStyle = classNames({
-    [style.title]: true,
+  const titleStyle = classNames(style.title, {
     [style.longTitle]: event.title.length >= 25,
     [style.shortTitle]: event.title.length < 25,
   });
@@ -76,27 +74,19 @@ export const EventCardElement = ({ eventId, event }: IProps) => {
     event,
     waitingListSpot: hasLoaded(waitingListSpot)
       ? waitingListSpot.data
-      : 'loading',
+      : 'Laster',
     registrationState,
     editToken,
   });
 
-  console.log(event.title, ' ', eventState);
   return (
     <Link to={viewRoute} className={style.link}>
-      <div
-        className={style.card}
-        onMouseEnter={() => setSvgColor('Black')}
-        onMouseLeave={() => setSvgColor('White')}
-      >
+      <div className={style.card}>
         <div className={style.date}>{dateTimeText}</div>
         <div className={titleStyle}>{event.title}</div>
         <div className={style.location}>
-          {' '}
           <div className={style.locationIcon}>
-            <span>
-              <LocationIconComponent color={svgColor} />
-            </span>
+            <LocationIcon />
           </div>
           <div className={style.locationText}>{event.location}</div>
         </div>
@@ -118,37 +108,12 @@ export const EventCardElement = ({ eventId, event }: IProps) => {
   );
 };
 
-const LocationIconComponent = (props: { color: string }) => {
-  return (
-    <svg
-      width="9"
-      height="20"
-      viewBox="0 0 9 20"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <g clip-path="url(#clip0)">
-        <path
-          d="M3.92494 6.33334V15.3583H4.63328V6.33334C5.35244 6.25884 6.02371 5.93793 6.53328 5.425C6.97591 4.98042 7.27691 4.41467 7.39829 3.79916C7.51967 3.18365 7.45598 2.54598 7.21526 1.96664C6.97454 1.3873 6.56758 0.892257 6.04576 0.544001C5.52394 0.195746 4.91064 0.0098877 4.28328 0.0098877C3.65592 0.0098877 3.04262 0.195746 2.5208 0.544001C1.99897 0.892257 1.59202 1.3873 1.3513 1.96664C1.11058 2.54598 1.04689 3.18365 1.16827 3.79916C1.28964 4.41467 1.59064 4.98042 2.03328 5.425C2.54069 5.93621 3.20873 6.25698 3.92494 6.33334ZM2.49994 1.43334C2.84707 1.07754 3.29238 0.833304 3.77899 0.731824C4.2656 0.630344 4.77142 0.676229 5.23183 0.863618C5.69223 1.05101 6.08632 1.37139 6.36376 1.78384C6.64119 2.1963 6.78938 2.68209 6.78938 3.17917C6.78938 3.67625 6.64119 4.16205 6.36376 4.5745C6.08632 4.98695 5.69223 5.30734 5.23183 5.49473C4.77142 5.68211 4.2656 5.728 3.77899 5.62652C3.29238 5.52504 2.84707 5.2808 2.49994 4.925C2.04438 4.45806 1.78938 3.83154 1.78938 3.17917C1.78938 2.52681 2.04438 1.90028 2.49994 1.43334Z"
-          fill={props.color}
-        />
-        <path d="M8.56667 19.2917H0V20H8.56667V19.2917Z" fill={props.color} />
-      </g>
-      <defs>
-        <clipPath id="clip0">
-          <rect width="8.56667" height="20" fill={props.color} />
-        </clipPath>
-      </defs>
-    </svg>
-  );
-};
-
 interface EventStateProps {
   eventId: string;
   event: IEvent;
   editToken?: string;
-  waitingListSpot: number | 'ikke-påmeldt' | 'loading';
-  registrationState?: 'Plass' | 'Plass på venteliste' | 'loading' | 'Fullt';
+  waitingListSpot: number | 'ikke-påmeldt' | 'Laster';
+  registrationState?: 'Plass' | 'Plass på venteliste' | 'Laster' | 'Fullt';
 }
 
 const useGetState = ({
--- app/src/components/ViewEventsCards/EventState.tsx
+++ app/src/components/ViewEventsCards/EventState.tsx
@@ -16,7 +16,7 @@ export type eventState =
   | 'På venteliste'
   | 'Plass'
   | 'Plass på venteliste'
-  | 'loading'
+  | 'Laster'
   | undefined;
 interface IProps {
   eventId: string;
@@ -77,14 +77,14 @@ export const EventState = ({
     case 'Plass på venteliste':
       return (
         <div>
-          <div className={style.stateText}>Arrangementet er fult.</div>
+          <div className={style.stateText}>Arrangementet er fullt.</div>
           <LinkButton to={viewEventRoute(eventId)}>
             Sett deg på venteliste
           </LinkButton>
         </div>
       );
 
-    case 'loading':
+    case 'Laster':
       return <div>Laster</div>;
 
     case undefined:
@@ -97,41 +97,5 @@ const LinkButton = (props: { to: string; children: ReactChild }) => {
     <Link to={props.to} className={style.linkButton}>
       {props.children}
     </Link>
-  );
-};
-
-const SmileIcon = () => {
-  return (
-    <svg
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <g clip-path="url(#clip0)">
-        <path
-          d="M12 0C5.4 0 0 5.4 0 12C0 18.6 5.4 24 12 24C18.6 24 24 18.6 24 12C24 5.4 18.6 0 12 0ZM12 22.8C6 22.8 1.2 18 1.2 12C1.2 6 6 1.2 12 1.2C18 1.2 22.9 6 22.9 12C22.9 18 18 22.8 12 22.8Z"
-          fill="white"
-        />
-        <path
-          d="M12.0001 17.3C10.2001 17.3 8.8001 15.8 8.8001 14.1H7.6001C7.6001 16.6 9.6001 18.5 12.0001 18.5C14.4001 18.5 16.4001 16.5 16.4001 14.1H15.2001C15.2001 15.9 13.8001 17.3 12.0001 17.3Z"
-          fill="white"
-        />
-        <path
-          d="M8.5 8C8.5 7.4 8 7 7.5 7C6.9 7 6.5 7.5 6.5 8C6.5 8.6 7 9 7.5 9C8.1 9.1 8.5 8.6 8.5 8Z"
-          fill="white"
-        />
-        <path
-          d="M16.5 9C17.0523 9 17.5 8.55228 17.5 8C17.5 7.44772 17.0523 7 16.5 7C15.9477 7 15.5 7.44772 15.5 8C15.5 8.55228 15.9477 9 16.5 9Z"
-          fill="white"
-        />
-      </g>
-      <defs>
-        <clipPath id="clip0">
-          <rect width="24" height="24" fill="white" />
-        </clipPath>
-      </defs>
-    </svg>
   );
 };
