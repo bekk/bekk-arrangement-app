bekk/bekk-arrangement-app

commit 9e1f73b4a610af085f8e3bdf62b57f69d16be8fb

Add PrivateRouteComponent

app/src/components/App/App.tsx
@@ -1,7 +1,8 @@
-import { useAuth0Redirect } from 'src/auth';
-import { Router, Switch, Route, Redirect } from 'react-router';
+import { authenticateUser, isAuthenticated, redirectToAuth0, useAuth0Redirect } from 'src/auth';
+import { Router, Switch, Route, Redirect, RouteProps } from 'react-router';
 import React from 'react';
 import { Header } from 'src/components/Common/Header/Header';
+import { ReactChild } from 'src/types';
 import {
   createRoute,
   viewEventRoute,
@@ -41,14 +42,14 @@ export const App = () => {
       <div className={style.container}>
         <Header />
         <Switch>
-          <Route exact path={createRoute}>
-            <CreateEventContainer />
-          </Route>
           <Route exact path={viewEventRoute(':' + eventIdKey)}>
             <ViewEventContainer />
           </Route>
-          <Route exact path={eventsRoute}>
+          <PrivateRoute exact path={eventsRoute}>
             <ViewEventsCardsContainer />
+          </PrivateRoute>
+          <Route exact path={createRoute}>
+              <CreateEventContainer />
           </Route>
           <Route path={editEventRoute(':' + eventIdKey)}>
             <EditEventContainer />
@@ -82,3 +83,22 @@ export const App = () => {
     </Router>
   );
 };
+export type ProtectedRouteProps = {
+  children: ReactChild;
+} & RouteProps
+
+
+const PrivateRoute = ({children, ...routeProps}:ProtectedRouteProps) => {
+  return (
+    <Route
+      {...routeProps}
+      render={({ location }) =>
+        isAuthenticated() ? (
+          children
+        ) : (
+          <> {redirectToAuth0()} </>
+        )
+      }
+    />
+  );
+}

app/src/auth.ts
@@ -86,7 +86,7 @@ function getAuth0Url(): string {
   return `https://${getIssuerDomain()}/authorize?client_id=${getAudience()}&response_type=token&redirect_uri=${encodedCallback}&scope=${encodedScopes}&state=${state}`;
 }
 
-function redirectToAuth0(): void {
+export function redirectToAuth0(): void {
   const url = getAuth0Url();
   window.location.replace(url);
 }

commit 772a091b3cc1a9e3da672e6e32a579b9568310be

revert bug introduced in 9e1f73b, changing the route order

app/src/components/App/App.tsx
@@ -42,14 +42,14 @@ export const App = () => {
       <div className={style.container}>
         <Header />
         <Switch>
+          <PrivateRoute exact path={createRoute}>
+            <CreateEventContainer />
+          </PrivateRoute>
           <Route exact path={viewEventRoute(':' + eventIdKey)}>
             <ViewEventContainer />
           </Route>
           <PrivateRoute exact path={eventsRoute}>
             <ViewEventsCardsContainer />
-          </PrivateRoute>
-          <PrivateRoute exact path={createRoute}>
-            <CreateEventContainer />
           </PrivateRoute>
           <PrivateRoute path={editEventRoute(':' + eventIdKey)}>
             <EditEventContainer />