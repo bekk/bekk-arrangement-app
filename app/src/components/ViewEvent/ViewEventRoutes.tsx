66e6010d34a92a781e3f9676b229eb4367faf26f
tarjei <tarjei.skjaerset@gmail.com>
2021-07-20 12:12:27 +0200

    Add 404 page for missing shortname

--- app/src/components/ViewEvent/ViewEventRoutes.tsx
+++ app/src/components/ViewEvent/ViewEventRoutes.tsx
@@ -4,6 +4,7 @@ import { ViewEventContainer } from 'src/components/ViewEvent/ViewEventContainer'
 import { useEffectOnce } from 'src/hooks/utils';
 import { eventIdKey, shortnameKey } from 'src/routing';
 import { useParam } from 'src/utils/browser-state';
+import style from './ViewEventContainer.module.scss';
 
 export const ViewEventContainerRegularRoute = () => {
   const eventId = useParam(eventIdKey);
@@ -13,14 +14,28 @@ export const ViewEventContainerRegularRoute = () => {
 export const ViewEventShortnameRoute = () => {
   const shortname = useParam(shortnameKey);
 
-  const [eventId, setEventId] = useState<string>();
+  const [eventId, setEventId] = useState<string | null>();
   useEffectOnce(async () => {
-    const eventId = await getEventIdByShortname(shortname);
-    setEventId(eventId);
+    try {
+      const eventId = await getEventIdByShortname(shortname);
+      setEventId(eventId);
+    } catch {
+      setEventId(null);
+    }
   });
 
   if (eventId === undefined) {
     return null;
+  }
+
+  if (eventId === null) {
+    return (
+      <div className={style.fourOhFour}>
+        <div>
+          <span>404</span> Det finnes ingen arrangement med navn "{shortname}"
+        </div>
+      </div>
+    );
   }
 
   return <ViewEventContainer eventId={eventId} />;
