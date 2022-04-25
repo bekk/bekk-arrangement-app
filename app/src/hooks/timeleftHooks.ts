2486214baf3bc791bf6a922321c02600543fef63
Bjørn-Ivar Strøm <bjorn.ivar.strom@bekk.no>
2022-04-25 08:15:00 +0200

    Rerendering (#103)
    Only rerender event-view if it is less than 12 hours before registration opens.

--- app/src/hooks/timeleftHooks.ts
+++ app/src/hooks/timeleftHooks.ts
@@ -6,15 +6,21 @@ export const useTimeLeft = (time: Date | false) => {
     calculateTimeLeft(time || new Date())
   );
 
+  const isOpenForRegistration = timeLeft.difference <= 0
+  const registrationIsWithin12Hours = timeLeft.difference < (60 * 60000 * 12)
+
   useEffect(() => {
-    if (time) {
+    if (isOpenForRegistration)
+      return;
+
+    if (time && registrationIsWithin12Hours) {
       setTimeLeft(calculateTimeLeft(time));
       const interval = setInterval(() => {
         setTimeLeft(calculateTimeLeft(time));
       }, 500);
       return () => clearInterval(interval);
     }
-  }, [time]);
+  }, [time, isOpenForRegistration, registrationIsWithin12Hours]);
 
   return timeLeft;
 };
